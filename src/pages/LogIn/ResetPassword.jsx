1bacacb56746246b9c4b93bedd7db900979144cf
mehedy201 <mehedy643@gmail.com>
2025-07-18 22:06:25 +0600

    complete reset password page

--- src/pages/LogIn/ResetPassword.jsx
+++ src/pages/LogIn/ResetPassword.jsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
 import "./logIn.css";
 import { useNavigate } from "react-router-dom";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.config";
+import { useForm } from "react-hook-form";
 function ResetPassword() {
-  const [isResetPassword, setIsResetPassword] = useState(false);
-
-  const navigate = useNavigate();
+  
+  const navigate = useNavigate()
+  
+    const [sendPasswordResetEmail] = useSendPasswordResetEmail(auth);
+    const [isResetPassword, setIsResetPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [emaliErr, setEmailErr] = useState();
+    // User Update Form _________________________________
+    const { register, handleSubmit, formState: { errors }} = useForm();
+    const onSubmit = async (data) => {
+        setLoading(true)
+        const email = data.email
+        sendPasswordResetEmail(email)
+        .then((res) => {
+            console.log(res);
+            setIsResetPassword(true)
+        })
+        .catch(err => {
+            console.log(err);
+            setEmailErr(err)
+        })
+        setLoading(false)   
+    }
 
 
   return (
@@ -16,26 +39,35 @@ function ResetPassword() {
         </div>
         <h5> We will email you instructions on how to reset your password.</h5>
 
-        {isResetPassword ? (
-          <p className="resetPass-msg">
-            We have sent you an email with instructions on how to reset your
-            password.
-          </p>
-        ) : (
-          <>
-            <label>Email</label>
-
-            <input type="text" name="firstName" />
-
-            <button
-              className="theme-btn"
-              style={{ width: "100%", margin: "0 0 24px 0" }}
-              onClick={() => setIsResetPassword(true)}
-            >
-              Reset Password
-            </button>
-          </>
-        )}
+        <div>
+            {isResetPassword ? (
+            <p className="resetPass-msg">
+              We have sent you an email with instructions on how to reset your
+              password.
+            </p>
+          ) : (
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <label>Email</label>
+              <input {...register("email", { required: true})} type="email" />
+              {errors.email && <span style={{color: 'red'}}>Please enter your email</span>}
+              <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              {
+                loading && <p>Loading.....</p>
+              }
+              {
+                emaliErr && <p style={{color: 'red'}}>{emaliErr}</p>
+              }
+              </div>
+              <button
+                className="theme-btn"
+                style={{ width: "100%", margin: "0 0 24px 0" }}
+                type="submit"
+              >
+                Reset Password
+              </button>
+            </form>
+          )}
+        </div>
 
         <button onClick={() => navigate('/login')} className="theme-btn2">Return to login</button>
       </div>
