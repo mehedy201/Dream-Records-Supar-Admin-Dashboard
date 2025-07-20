de8a29f1bd631877e1a22c045ff348b5a4143cca
mehedy201 <mehedy643@gmail.com>
2025-07-20 12:22:56 +0600

    fix sidebar icon and logo issu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,34 @@
 import { NavLink } from "react-router-dom";
 import "./Global.css";
+import logo from '../assets/Logo.png'
+
+import homeIcon from '../assets/icons/home.png'
+import userIcon from '../assets/icons/users.png'
+import distributionIcon from '../assets/icons/distribution.png'
+import groupIcon from '../assets/icons/group.png'
+import labelsIcon from '../assets/icons/Labels.png'
+import analyticsIcon from '../assets/icons/analytics.png'
+import serviceRequestIcon from '../assets/icons/Service Request.png'
+import transactionsIcon from '../assets/icons/Transactions.png'
+import supportIcon from '../assets/icons/Support.png'
+import helpIcon from '../assets/icons/Help.png'
+import profileIcon from '../assets/icons/Profile.png'
+import settingsIcon from '../assets/icons/Settings.png'
+
+
 const menuItems = [
-  { name: "Home", path: "/", icon: "home.png" },
-  { name: "Users", path: "/users/Pending/1/10", icon: "users.png" },
-  { name: "Distribution", path: "/distribution", icon: "distribution.png" },
-  { name: "Artists", path: "/artists", icon: "group.png" },
-  { name: "Labels", path: "/Labels", icon: "Labels.png" },
-  { name: "Analytics", path: "/analytics/1/10", icon: "analytics.png" },
-  {
-    name: "Service Request",
-    path: "/ServiceRequest",
-    icon: "Service Request.png",
-  },
-  { name: "Transactions", path: "/Transaction", icon: "Transactions.png" },
-  { name: "Support", path: "/support", icon: "Support.png", divider: true },
-  { name: "Help", path: "/help", icon: "Help.png" },
-  { name: "Profile", path: "/profile", icon: "Profile.png", divider: true },
-  { name: "My Settings", path: "/settings", icon: "Settings.png" },
+  { name: "Home", path: "/", icon: homeIcon },
+  { name: "Users", path: "/users/Pending/1/10", icon: userIcon },
+  { name: "Distribution", path: "/distribution", icon: distributionIcon },
+  { name: "Artists", path: "/artists", icon: groupIcon },
+  { name: "Labels", path: "/Labels", icon: labelsIcon },
+  { name: "Analytics", path: "/analytics/1/10", icon: analyticsIcon },
+  { name: "Service Request", path: "/ServiceRequest",icon: serviceRequestIcon,},
+  { name: "Transactions", path: "/Transaction", icon: transactionsIcon },
+  { name: "Support", path: "/support", icon: supportIcon, divider: true },
+  { name: "Help", path: "/help", icon: helpIcon },
+  { name: "Profile", path: "/profile", icon: profileIcon, divider: true },
+  { name: "My Settings", path: "/settings", icon: settingsIcon },
 ];
 
 const Sidebar = () => {
@@ -24,7 +36,7 @@ const Sidebar = () => {
     <aside className="sidebar">
       {/* Logo */}
       <div className="sidebar-logo">
-        <img src="src/assets/Logo.png" alt="Dream Records Logo" width={86} />
+        <img src={logo} alt="Dream Records Logo" width={86} />
       </div>
 
       {/* Create Button */}
@@ -43,7 +55,7 @@ const Sidebar = () => {
               }
             >
               <img
-                src={`src/assets/icons/${item.icon}`}
+                src={item?.icon}
                 alt={item.name}
                 className="nav-icon"
               />
